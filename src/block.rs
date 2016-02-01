f740ca027ad1992c8274cb457f1c7f28539f97f7
Itai Bar-Natan <itaibn3141@gmail.com>
2016-02-01 06:31:07 -0500

    Change names Block -> BlockLink HeapBlock -> Block

--- src/block.rs
+++ src/block.rs
@@ -15,24 +15,24 @@ use std::mem;
 // unsafe Rust to simulate such a feature, and I'm not convinced my
 // implementation is safe.
 //
-// The type HeapBlock<'a> corresponds to a block with all references having
+// The type Block<'a> corresponds to a block with all references having
 // lifetime 'a (including all references in blocks that it references,
-// recursively). Since HeapBlock<'a> includes cache data with references to
+// recursively). Since Block<'a> includes cache data with references to
 // other blocks and interior mutability, it is invariant in 'a.
 
 // A hashtable with all the blocks used for a Hashlife computation.
 pub struct CABlockCache (HashMap<u64, UnsafeBlock>);
 
-struct UnsafeBlock(HeapBlock<'static>);
+struct UnsafeBlock(Block<'static>);
 
 impl CABlockCache {
     pub fn new() -> Self {CABlockCache (HashMap::new())}
 
-    pub fn get_block<'a>(&'a mut self, desc: BlockDesc<'a>) -> Block<'a> {
+    pub fn get_block<'a>(&'a mut self, desc: BlockDesc<'a>) -> BlockLink<'a> {
         let hash = hash(&desc);
         let unsafe_block = self.0.entry(hash).or_insert_with(||
             UnsafeBlock::from_heap_block(
-                HeapBlock::from_desc_and_hash(desc, hash)
+                Block::from_desc_and_hash(desc, hash)
             )
         );
         unsafe {&unsafe_block.to_heap_block()}
@@ -40,34 +40,34 @@ impl CABlockCache {
 }
 
 impl UnsafeBlock {
-    fn from_heap_block(heap_block: HeapBlock) -> UnsafeBlock {
+    fn from_heap_block(heap_block: Block) -> UnsafeBlock {
         unsafe {UnsafeBlock(mem::transmute(heap_block))}
     }
 
-    unsafe fn to_heap_block<'a>(&self) -> &HeapBlock<'a> {
+    unsafe fn to_heap_block<'a>(&self) -> &Block<'a> {
         mem::transmute(&self.0)
     }
 }
 
-pub struct HeapBlock<'a> {
+pub struct Block<'a> {
     content: BlockDesc<'a>,
     hash: u64,
-    evolve: Cache<Block<'a>>,
+    evolve: Cache<BlockLink<'a>>,
 }
 
 #[derive(Hash)]
 pub enum BlockDesc<'a> {
-    Node([[Block<'a>; 2]; 2]),
+    Node([[BlockLink<'a>; 2]; 2]),
     Leaf(Leaf),
 }
 
-pub type Block<'a> = &'a HeapBlock<'a>;
+pub type BlockLink<'a> = &'a Block<'a>;
 pub type Leaf = u8;
 
 
-impl<'a> HeapBlock<'a> {
-    fn from_desc_and_hash(desc: BlockDesc, hash: u64) -> HeapBlock {
-        HeapBlock {
+impl<'a> Block<'a> {
+    fn from_desc_and_hash(desc: BlockDesc, hash: u64) -> Block {
+        Block {
             content: desc,
             hash: hash,
             evolve: Cache::new(),
@@ -75,7 +75,7 @@ impl<'a> HeapBlock<'a> {
     }    
 }
 
-impl<'a> Hash for HeapBlock<'a> {
+impl<'a> Hash for Block<'a> {
     fn hash<H:Hasher>(&self, state: &mut H) {
         state.write_u64(self.hash);
     }
