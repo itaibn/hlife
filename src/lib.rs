96b9224a52590a795cb6e6b3a816c9e372454f8f
Itai Bar-Natan <itaibn3141@gmail.com>
2016-02-29 12:17:15 -0500

    Add pedantic clippy option

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #![cfg_attr(feature="clippy", feature(plugin))]
 #![cfg_attr(feature="clippy", plugin(clippy))]
 
+#![cfg_attr(feature="clippy_pedantic", warn(clippy_pedantic))]
+
 // Clippy doesn't like this pattern, but I do. I may consider changing my mind
 // on this in the future, just to make clippy happy.
 #![cfg_attr(feature="clippy", allow(needless_range_loop))]
