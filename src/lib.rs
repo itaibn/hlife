ae57cecee93314db66f1f265896f4d70f5b5d9e8
Itai Bar-Natan <itaibn3141@gmail.com>
2016-03-02 16:09:01 -0500

    Make clippy_pedantic more pedantic

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,8 @@
 
 // Clippy doesn't like this pattern, but I do. I may consider changing my mind
 // on this in the future, just to make clippy happy.
-#![cfg_attr(feature="clippy", allow(needless_range_loop))]
+#![cfg_attr(all(feature="clippy", not(feature="clippy_pedantic")),
+    allow(needless_range_loop))]
 
 #[macro_use]
 extern crate nom;
