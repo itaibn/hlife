765e4b29c7a4ad05b1de0d819d22a4232cf22b40
Itai Bar-Natan <itaibn3141@gmail.com>
2016-07-09 17:03:59 -0400

    `block_from_bytes` reads .mc

--- src/format/mod.rs
+++ src/format/mod.rs
@@ -10,15 +10,20 @@ impl<'a> Hashlife<'a> {
     pub fn block_from_bytes(&self, bytes: &[u8]) -> Result<Block<'a>, ()> {
         use self::parse::{parse_file, ParseOut};
         use self::build_rle::block_from_rle;
+        use self::build_mc::build_mc;
         use nom::IResult;
 
         // TODO: Do this with less copying.
         let mut with_newline = bytes.to_vec();
         with_newline.push(b'\n');
 
-        if let IResult::Done(_, ParseOut::RLE(tokens)) =
+        if let IResult::Done(_, parse_out) =
                 parse_file(&with_newline) {
-            block_from_rle(self, &tokens)
+            match parse_out {
+                ParseOut::RLE(tokens) => block_from_rle(self, &tokens),
+                ParseOut::MC(lines) => build_mc(self, &lines),
+                ParseOut::Fail => Err(()),
+            }
         } else {
             Err(())
         }
@@ -41,5 +46,9 @@ fn test_block_from_bytes() {
         assert_eq!(hl.block_from_bytes(b"3o"), Err(()));
         let double_header = b"x=2,y=2,rule=B3/S23\nx=2,y=2,rule=B3/S23\nbb$bb!";
         assert_eq!(hl.block_from_bytes(double_header), Err(()));
+
+        // .mc
+        assert_eq!(hl.block_from_bytes(b"x=8,y=8\nbo$2bo$3o2$3o$2bo$bo!"),
+                   hl.block_from_bytes(b"[M2]\n.*$..*$***$$***$..*$.*$$\n"));
     });
 }
