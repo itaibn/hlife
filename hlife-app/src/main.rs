0c34b8567e9410dee0d359817a53cc31826b4f79
Itai Bar-Natan <itaibn3141@gmail.com>
2017-10-19 11:01:45 -0400

    Incorporate clap

--- hlife-app/src/main.rs
+++ hlife-app/src/main.rs
@@ -1,39 +1,49 @@
+extern crate clap;
+
 extern crate hlife;
+
+use std::fs::File;
+use std::io::Read;
+use std::process::exit;
+
+use clap::{Arg, App};
 
 use hlife::Hashlife;
 use hlife::global::Pattern;
 use hlife::format::write::format_rle;
 
-use std::env::args;
-use std::fs::File;
-use std::io::Read;
-use std::process::exit;
-
 fn main() {
-    let args: Vec<_> = args().collect();
-    if args.len() != 3 {
-        println!("{} input.rle gens", &args[0]);
-        exit(1);
-    }
-    let gens = u64::from_str_radix(&args[2], 10).unwrap_or_else(|_| {
+    let matches = App::new("Itai's Hashlife")
+            .arg(Arg::with_name("INPUT-FILE")
+                    .required(true)
+                    .index(1))
+            .arg(Arg::with_name("GENERATIONS")
+                    .required(true)
+                    .index(2))
+            .get_matches();
+
+    let filename = matches.value_of("INPUT-FILE").expect("internal clap error");
+    let gens_string = matches.value_of("GENERATIONS").expect("internal clap\
+        error");
+    let gens = u64::from_str_radix(&gens_string, 10).unwrap_or_else(|_| {
         println!(
             "Error: Second argument gens must be a nonnegative integer: {}",
-            &args[2]);
+            &gens_string);
         exit(1);
     });
-    let mut in_file = File::open(&args[1]).unwrap_or_else(|_| {
-        println!("Cannot open file {}", &args[1]);
+    let mut in_file = File::open(&filename).unwrap_or_else(|_| {
+        println!("Cannot open file {}", &filename);
         exit(1);
     });
     let mut rle_buf = Vec::new();
     in_file.read_to_end(&mut rle_buf).unwrap_or_else(|_| {
-        println!("Error reading file {}", &args[1]);
+        println!("Error reading file {}", &filename);
         exit(1);
     });
 
     Hashlife::with_new(|hl| {
         let block = hl.block_from_bytes(&rle_buf).unwrap_or_else(|_| {
-            println!("Badly formatted RLE in {}", &args[1]);
+            println!("Badly formatted RLE in {}", &filename);
             exit(1);
         });
         let mut pattern = Pattern::new(block);
